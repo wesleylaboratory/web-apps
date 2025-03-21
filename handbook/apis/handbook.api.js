a32c0e899ee3ebb287bdb722a2016b1366a13e88
wesleylaboratory <wesleylaboratory@gmail.com>
2025-03-21 00:04:19 -0500

    Name change

--- handbook/apis/handbook.api.js
+++ handbook/apis/handbook.api.js
@@ -1,4 +1,4 @@
-export function useLaboratoryApi() {
+export function useHandbookApi() {
     const baseUrl = 'https://script.google.com/macros/s/AKfycbxlOksq0mEwh1ve6yBnxjEO1W_ai-ypOGvl7da9YQ_WZ3LSyisbTTXln5ayCq086-yy/exec';
 
     /**
