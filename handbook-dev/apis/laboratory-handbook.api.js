c6c275adf88c363c88109a877bf8065cd83482ba
wesleylaboratory <wesleylaboratory@gmail.com>
2025-03-20 23:21:17 -0500

    Pulling libs internally and setting up api

--- handbook-dev/apis/laboratory-handbook.api.js
+++ handbook-dev/apis/laboratory-handbook.api.js
@@ -1,5 +1,31 @@
 export function useLaboratoryApi() {
     const baseUrl = 'https://script.google.com/macros/s/AKfycbxlOksq0mEwh1ve6yBnxjEO1W_ai-ypOGvl7da9YQ_WZ3LSyisbTTXln5ayCq086-yy/exec';
-    
 
+    /**
+     * @param {string} [query]
+     * @param {{limit?: number, page?: number, sort?: number, direction?: string, type?: string}} [options]
+     * @returns {boolean}
+     */
+    async function getHandbookFiles(query, options = null) {
+        /** @type {Array<IHandbook>} */
+        let handbookRes = [];
+
+        try {
+            const params = new URLSearchParams({
+                query: (query ?? ''), 
+                ...(options ?? {})
+            }).toString();
+            const res = await fetch(`${baseUrl}?${params}`);
+            handbookRes = res.json();
+        } catch (e) {
+            // Probably show a bootstrap toast message when this happens
+            console.error(e);
+        } finally {
+            return handbookRes;
+        }
+    }
+
+    return {
+        getHandbookFiles
+    };
 }
---
+++ handbook-dev/types/handbook.type.js
@@ -0,0 +1,11 @@
+/**
+ * @typedef {Object} IHandbook
+ * @property {string} id
+ * @property {string} name
+ * @property {string} url
+ * @property {string} type
+ * @property {string} size
+ * @property {string} createdDate
+ * @property {string} lastUpdated
+ * @property {string} description
+ */
