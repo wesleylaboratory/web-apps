44264b120f29e4934dd9af05305f575587513750
Anthony D'Alessio <anthony.m.dalessio@gmail.com>
2025-03-25 20:48:41 -0500

    Testing background in dev version

---
+++ handbook-dev/utils/time.util.js
@@ -0,0 +1,14 @@
+import { isNullOrEmpty, hashString } from "./string.util.js";
+
+/**
+ *
+ * @param {Function} callback
+ * @param {number} delay
+ * @param {string} [id]
+ */
+export function debounce(callback, delay, id) {
+    const identifier = isNullOrEmpty(id) ? id : hashString(JSON.stringify(callback));
+    window.clearTimeout(window[`debounce${identifier}`]);
+
+    window[`debounce${identifier}`] = window.setTimeout(callback, delay);
+}
