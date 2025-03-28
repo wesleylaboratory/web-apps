6cfa223499cb1eda6c89a93bf87dae094c600b09
wesleylaboratory <wesleylaboratory@gmail.com>
2025-03-28 18:33:27 -0500

    Update handbook.api.js

--- handbook-dev/apis/handbook.api.js
+++ handbook-dev/apis/handbook.api.js
@@ -33,7 +33,7 @@ export function useHandbookApi() {
      */
     async function getHandbookFilesFromIndex() {
         try {
-            const res = await fetch('https://raw.githubusercontent.com/wesleylaboratory/indexed-files/refs/heads/main/files.json');
+            const res = await fetch('https://raw.githubusercontent.com/wesleylaboratory/indexed-files/refs/heads/main/handbook/files.json');
 
             /** @type {Array<IHandbookFile>} */
             const filesRes = await res.json();
