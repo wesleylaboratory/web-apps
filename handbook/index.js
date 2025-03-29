5ad1f3bad7becf975f639dbe68e84e82afc257f8
Anthony D'Alessio <anthony.m.dalessio@gmail.com>
2025-03-29 18:46:36 -0500

    Some styling updates to include date updated

--- handbook/index.js
+++ handbook/index.js
@@ -14,11 +14,17 @@ let allFiles = [];
         const filterList = filteredListBySearch(searchTerm, allFiles);
         updateDomList(filterList, unorderedListElem);
 
-        const noResultsElem = document.querySelector('#no-results');
-        if (filterList.length <= 0 && searchTerm.length > 0) {
-            noResultsElem.classList.remove('d-none');
+        const resultsCountElem = document.querySelector('#results-count');
+        if (searchTerm.length <= 0) {
+            resultsCountElem.classList.add('d-none');
         } else {
-            noResultsElem.classList.add('d-none');
+            resultsCountElem.classList.remove('d-none');
+            if (filterList.length > 0) {
+                const pluralisedResultLabel = filterList.length === 1 ? 'result' : 'results';
+                resultsCountElem.innerHTML = `${filterList.length} ${pluralisedResultLabel} found.`;
+            } else {
+                resultsCountElem.innerHTML = 'No results found matching search.';
+            }
         }
     });
 
@@ -44,7 +50,7 @@ function filteredListBySearch(value, list) {
         if (value.length === 1) {
             return l.name != null && l.name.trim().toLowerCase().substr(0, 1) === value.toLowerCase();
         } else {
-           return l.name != null && l.name.trim().toLowerCase().includes(value.toLowerCase());
+            return l.name != null && l.name.trim().toLowerCase().includes(value.toLowerCase());
         }
     });
 }
@@ -56,15 +62,33 @@ function updateDomList(filteredList, unorderedListElem) {
 
     unorderedListElem.innerHTML = '';
 
+    if (filteredList.length <= 0) {
+        unorderedListElem.classList.add('d-none');
+    } else {
+        unorderedListElem.classList.remove('d-none');
+    }
+
     for (const file of filteredList) {
         const li = document.createElement('li');
         li.classList.add('list-group-item', 'list-group-item-action');
 
+        const formattedDate = file.lastUpdated ? new Date(file.lastUpdated).toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric'
+        }) : undefined;
         const encodedStr = `
-            <a target="_blank" href="${file.url}" class="text-truncate d-block w-100 h-100">
-                ${encodeHtmlString(file.name)}
+            <a target="_blank" href="${file.url}" class="d-flex flex-row text-decoration-none text-black align-items-center gap-2">
+                <div class="d-flex flex-column">
+                    <span class="fw-bold">${encodeHtmlString(file.name)}</span>
+                    <small class="fw-lighter text-secondary" style="font-size: 13px">${'Updated on ' + formattedDate}</small>
+                </div>
             </a>
         `;
+
+        // <span class="material-symbols-outlined text-secondary">docs</span>
 
         li.innerHTML = encodedStr;
         unorderedListElem.appendChild(li);
